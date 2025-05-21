b8988f98284888b5c112446a7c5f7fa84529ca8e
Haruumi Kondo <other.hk@gmail.com>
2025-05-22 01:55:24 +0900

    add autogenerate_revision_name

--- terraform/modules/cloud_run_service/main.tf
+++ terraform/modules/cloud_run_service/main.tf
@@ -1,6 +1,7 @@
 resource "google_cloud_run_service" "this" {
-  name     = var.service_name
-  location = var.region
+  name                       = var.service_name
+  location                   = var.region
+  autogenerate_revision_name = true # 自動でリビジョン末尾の識別文字列を入れるために必要
 
   template {
     spec {
