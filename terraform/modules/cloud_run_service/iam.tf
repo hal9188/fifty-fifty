df9dcb6d56be4dbd5bc10c7081d0824e768b0ed9
Haruumi Kondo <other.hk@gmail.com>
2025-05-24 16:10:50 +0900

    fix

--- terraform/modules/cloud_run_service/iam.tf
+++ terraform/modules/cloud_run_service/iam.tf
@@ -1,8 +1,7 @@
-resource "google_cloud_run_service_iam_member" "unauthenticated_invoker" {
-  location = var.location
-  project  = var.project
-  service  = google_cloud_run_v2_service.this.name
-
+resource "google_cloud_run_v2_service_iam_member" "unauthenticated_invoker" {
+  project = google_cloud_run_v2_service.this.project
+  location = google_cloud_run_v2_service.this.location
+  name = google_cloud_run_v2_service.this.name
   role   = "roles/run.invoker"
   member = "allUsers"
 }
