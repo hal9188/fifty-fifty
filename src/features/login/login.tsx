35dbf56219b50d34e193a487c57c46df13077bb7
Haruumi Kondo <other.hk@gmail.com>
2025-05-25 16:37:05 +0900

    Revert "test"
    This reverts commit 19dba027679e73da774c8a9e8c9cc964bbe69e66.

--- src/features/login/login.tsx
+++ src/features/login/login.tsx
@@ -59,7 +59,7 @@ export function Login() {
           render={({ field }) => (
             <FormItem>
               <FormLabel>パスワード</FormLabel>
-              <div className="absolute">
+              <div className="relative">
                 <FormControl>
                   <Input
                     type={showPassword ? 'text' : 'password'}
