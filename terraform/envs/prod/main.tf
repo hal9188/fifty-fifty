48d56a0b23440a8cfc58dd9e036d8c99fd8e23f1
Haruumi Kondo <other.hk@gmail.com>
2025-05-25 22:10:47 +0900

    fix setting

--- terraform/envs/prod/main.tf
+++ terraform/envs/prod/main.tf
@@ -5,7 +5,7 @@ provider "google" {
 
 module "fifty_fifty" {
   source        = "../../modules/cloud_run_service"
-  service_name  = var.service_name
+  service_name  = "${var.service_name}-prod"
   region        = var.region
   location      = var.region
   project       = var.project
